3f0c590172ed55b80541e4ee3e1f6168b7f0184a
Himanshu <hipr27052002@gmail.com>
2024-07-20 23:47:38 +0530

    initial

---
+++ src/components/header.tsx
@@ -0,0 +1,77 @@
+import { signOut } from "firebase/auth";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import {
+  FaSearch,
+  FaShoppingCart,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { auth } from "../firebase";
+import "../styles/app.scss";
+import { User } from "../types/types";
+
+interface PropsType {
+  user: User | null;
+}
+
+const Header = ({ user }: PropsType) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = async() => {
+    try{
+
+      await signOut(auth);
+      toast.success("Signed Out Successfully");
+      setIsOpen(false);
+    }
+    catch(error){
+      toast.error("Sign Out Fail");
+    }
+  };
+
+  return (
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+       HERBAL HAVEN
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingCart />
+      </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                  Admin
+                </Link>
+              )}
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
